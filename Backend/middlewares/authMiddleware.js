cff683ad1081dbec591c7696270ecdb8150b3df1
Anto jn <kuriyanjohn1953@gmail.com>
2025-05-23 12:59:07 +0530

    token issue solved'

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -4,17 +4,20 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
   console.log('Entering protect middleware');
   let token;
-
+  
+  try {
   const authHeader = req.headers.authorization;
-  if (authHeader && authHeader.startsWith('Bearer')) {
-    token = authHeader.split(' ')[1];
-  }
-
+  if (authHeader) {
+  token = authHeader.startsWith('Bearer') 
+    ? authHeader.split(' ')[1]
+    : authHeader;
+}
+  console.log('token',token);
+  
   if (!token) {
     return res.status(401).json({ message: 'Not authorized, token missing' });
   }
 
-  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id).select('-password');
 
@@ -22,11 +25,11 @@ const protect = async (req, res, next) => {
       return res.status(401).json({ message: 'User not found' });
     }
 
-    req.user = user;
+    req.user = user;  
     console.log('Authenticated user:', req.user);
-    // next();
+    next();
   } catch (err) {
-    console.error('Token error:', err.message);
+    console.log('Token error:', err.message);
     return res.status(401).json({ message: 'Token invalid or expired' });
   }
 };
@@ -37,7 +40,7 @@ const authorizeRoles = (...roles) => {
     if (!req.user || !roles.includes(req.user.role)) {
       return res.status(403).json({ message: 'Access denied: insufficient permissions' });
     }
-    // next();
+    next();
   };
 };
 
